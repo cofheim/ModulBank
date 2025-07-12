f4105bb8a433528821f0eab7c3b1adae0bf416ab
cofheim <plastininbiz@yandex.ru>
2025-07-12 16:08:30 +0300

    Добавьте файлы проекта.

---
+++ ModulBank/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace ModulBank
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var services = builder.Services;
+
+            // Add services to the container.
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
